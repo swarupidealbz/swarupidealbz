7a6b220143037323ed1e000e719eee5a4091340b
swarup <swarup@idealabz.biz>
2022-01-27 21:54:33 +0530

    fix image path name to upload

--- app/Http/Controllers/API/PrimaryTopicController.php
+++ app/Http/Controllers/API/PrimaryTopicController.php
@@ -67,7 +67,7 @@ class PrimaryTopicController extends BaseController
             }
 
             $imageName = time().'.'.$request->image->extension(); 
-            $path = $request->image->move('images', $imageName); //public/images/filename
+            $path = $request->image->move(asset('/images'), $imageName); //public/images/filename
             $inputData = [
                 'website_id' => $request->website,
                 'is_primary_topic' => $request->is_primary,
